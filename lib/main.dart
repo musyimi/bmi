e8067ba6890d98fdb6ea3c9e7ca18bc9824e267d
Musyimi <musyimi13@gmail.com>
2023-03-21 11:58:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMI());
+}
+
+class BMI extends StatelessWidget {
+  const BMI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+    );
+  }
+}
